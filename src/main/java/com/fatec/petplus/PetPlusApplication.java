6ea9b305751c2f44caa4967f667575eb76db4107
Marcelo <marceloabreu1997@gmail.com>
2022-09-02 10:37:07 -0300

    @m-arceloabreu: task: Project SEtup

---
+++ src/main/java/com/fatec/petplus/PetPlusApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.petplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetPlusApplication.class, args);
+	}
+
+}
